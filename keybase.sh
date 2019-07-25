1c4490b314fb251401058b9a38bb1cfcd6f56bc4
Yann Ponzoni <alphayax@gmail.com>
2019-07-25 17:51:06 +0200

    Add --wildcard for tar command

--- keybase.sh
+++ keybase.sh
@@ -106,7 +106,7 @@ sign() {
   chart=$1
   echo "Signing $chart"
   shasum=$(openssl sha1 -sha256 $chart| awk '{ print $2 }')
-  chartyaml=$(tar -zxf $chart --exclude 'charts/' -O '*/Chart.yaml')
+  chartyaml=$(tar --wildcards -zxf $chart --exclude 'charts/' -O '*/Chart.yaml')
 c=$(cat << EOF
 $chartyaml
 
